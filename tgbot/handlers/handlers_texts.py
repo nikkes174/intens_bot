bf0a55f34c36d48b87dc50e0742aec0cacb79ea7
nikkes174 <i.suldin@icloud.com>
2026-08-26 17:27:51 +0300

    two

--- tgbot/handlers/handlers_texts.py
+++ tgbot/handlers/handlers_texts.py
@@ -1,18 +1,18 @@
 ACCES_HANDLER = """
 Согласно законодательству РФ нам необходимо ваше согласие на получение писем от нас.
 Нажмите на кнопку «ПОДТВЕРЖДАЮ», чтобы мы могли прислать Вам сообщения в этом боте.
-Нажимая на кнопку, Вы подтверждаете своё <a href="https://docs.google.com/document/d/1FqGj5evgzR7fDqz32TPheFzfipPyJTZIYTg898BssO0/edit?tab=t.0#heading=h.qzz8f4p1hqkr">согласие
+Нажимая на кнопку, Вы подтверждаете своё <a href="https://docs.google.com/document/d/1ID2NEFT7d4rxd27FtZOol2ARwRwH4J9O5400u1fs3CA/edit?usp=drivesdk">согласие
 на обработку персональных данных</a> в соответствии с <a href="https://docs.google.com/document/d/1lyP1-tnLSjPQ2CgoP-G1Ys1LZxE2j5A9YofQB1Qtn4w/edit?usp=sharing">
-политикой обработки персональных данных</a> и даёте <a href="https://docs.google.com/document/d/1lR9sO7e-MQYXMXty3Zcdy3yPLMNKrtex6Hu1gchXvd4/edit?usp=sharing">
+политикой обработки персональных данных</a> и даёте <a href="https://docs.google.com/document/d/1TTBMphDiaEanhq1bXwknP57MmME2mNUoK5HaV3m68C0/edit?usp=drivesdk">
 согласие на получение информационных и рекламных сообщений.</a>
-Также Вы подтверждаете, что ознакомились с <a href="https://docs.google.com/document/d/1vXW8gvMgrfeRaGebfDpps10NVADUKyvukZNEGjOODLo/edit?usp=sharing">офертой</a>.
+Также Вы подтверждаете, что ознакомились с <a href="https://docs.google.com/document/d/1rkKrhXcQtj0XhCo51cxJ_y4Dkic6Po8YBJofVckG2fA/edit?usp=drivesdk">офертой</a>.
 Даёте согласие на запись голоса и изображения, а также на публикацию отзывов с использованием Ваших данных — <a href="https://docs.google.com/document/d/150u1pZpOtPnxpGFYjlpR2zG4T5OpXxrhb9qGNdn-3cY/edit?usp=sharing">ознакомиться с условиями</a>.
 
 """
 
 START_TEXT = """
-Интенсив "Проявленность, триггеры, продажи"\n
+Интенсив "Женская реализация в блоге"\n
 Продукт про продажи в социальных сетях за счет личного бренда и контента, клиенту обещается 2 офлайн встречи (при возможности клиента) и 5 онлайн созвонов, на них будет активная проработка страхов и стопоров, котгорые не дают в социальных сетях продавать, а также будут выдаваться механики триггеров и продаж
 
-стоимость 9990
+Стоимость 2990
 """
