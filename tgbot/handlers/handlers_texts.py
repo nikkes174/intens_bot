32c53440dd3229e6fc07000d77fac65f581a8abd
nikkes174 <i.suldin@icloud.com>
2026-09-01 08:37:32 +0300

    pr

--- tgbot/handlers/handlers_texts.py
+++ tgbot/handlers/handlers_texts.py
@@ -1,17 +1,29 @@
-ACCES_HANDLER = """
+
+
+
+
+LINK_CONSENT = "https://docs.google.com/document/d/1ID2NEFT7d4rxd27FtZOol2ARwRwH4J9O5400u1fs3CA/edit?usp=drivesdk"
+LINK_PRIVACY_POLICY = "https://docs.google.com/document/d/1lyP1-tnLSjPQ2CgoP-G1Ys1LZxE2j5A9YofQB1Qtn4w/edit?usp=sharing"
+LINK_MARKETING_CONSENT = "https://docs.google.com/document/d/1TTBMphDiaEanhq1bXwknP57MmME2mNUoK5HaV3m68C0/edit?usp=drivesdk"
+LINK_OFFER = "https://docs.google.com/document/d/1rkKrhXcQtj0XhCo51cxJ_y4Dkic6Po8YBJofVckG2fA/edit?usp=drivesdk"
+LINK_PUBLICITY = "https://docs.google.com/document/d/150u1pZpOtPnxpGFYjlpR2zG4T5OpXxrhb9qGNdn-3cY/edit?usp=sharing"
+
+
+ACCESS_HANDLER = f"""
 Согласно законодательству РФ нам необходимо ваше согласие на получение писем от нас.
 Нажмите на кнопку «ПОДТВЕРЖДАЮ», чтобы мы могли прислать Вам сообщения в этом боте.
-Нажимая на кнопку, Вы подтверждаете своё <a href="https://docs.google.com/document/d/1ID2NEFT7d4rxd27FtZOol2ARwRwH4J9O5400u1fs3CA/edit?usp=drivesdk">согласие
-на обработку персональных данных</a> в соответствии с <a href="https://docs.google.com/document/d/1lyP1-tnLSjPQ2CgoP-G1Ys1LZxE2j5A9YofQB1Qtn4w/edit?usp=sharing">
-политикой обработки персональных данных</a> и даёте <a href="https://docs.google.com/document/d/1TTBMphDiaEanhq1bXwknP57MmME2mNUoK5HaV3m68C0/edit?usp=drivesdk">
+Нажимая на кнопку, Вы подтверждаете своё <a href="{LINK_CONSENT}">согласие
+на обработку персональных данных</a> в соответствии с <a href="{LINK_PRIVACY_POLICY}">
+политикой обработки персональных данных</a> и даёте <a href="{LINK_MARKETING_CONSENT}">
 согласие на получение информационных и рекламных сообщений.</a>
-Также Вы подтверждаете, что ознакомились с <a href="https://docs.google.com/document/d/1rkKrhXcQtj0XhCo51cxJ_y4Dkic6Po8YBJofVckG2fA/edit?usp=drivesdk">офертой</a>.
-Даёте согласие на запись голоса и изображения, а также на публикацию отзывов с использованием Ваших данных — <a href="https://docs.google.com/document/d/150u1pZpOtPnxpGFYjlpR2zG4T5OpXxrhb9qGNdn-3cY/edit?usp=sharing">ознакомиться с условиями</a>.
-
+Также Вы подтверждаете, что ознакомились с <a href="{LINK_OFFER}">офертой</a>.
+Даёте согласие на запись голоса и изображения, а также на публикацию отзывов с использованием Ваших данных — <a href="{LINK_PUBLICITY}">ознакомиться с условиями</a>.
 """
 
 START_TEXT = """
-Челлендж «Перезагрузка блога»\n
-Короткий практический челлендж на 7 дней, чтобы перестать вести блог хаотично, найти своё направление и делать шаги в сторону понятного, живого и растущего блога.\n
-В программе: 3 групповых онлайн созвона, мини-уроки, практические задания, поддержка и обратная связь/общение в чате.
+Мини-курс "Кэшбек на стероидах. bySviat" 
+
+Короткий мини-курс из 7 модулей, который разложит по полочкам, как работает кэшбек и как можно максимизировать прибыль от него
+
+Каждый модуль - отдельное видео в канале, для каждого видео есть текствоая методичка. А также доступ в закрытый канал в Telegram и доступ к чату
 """
